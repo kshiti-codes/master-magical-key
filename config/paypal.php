4701e04a5752fdcdcc3cfe89875e3cc4626e0450
Kshiti Patel <kshiti.de@gmail.com>
2025-04-14 10:38:33 +0200

    paypal secret resolve

--- config/paypal.php
+++ config/paypal.php
@@ -10,12 +10,12 @@ return [
     ],
 
     'live' => [
-        'client_id'     => env('PAYPAL_LIVE_CLIENT_ID', 'Aagj8q7oz4BGfM3ikPkHQGjS3Kporqv93y6Hz2kDPqqbXXm9_KTmBeFfEvbEqYsHjgqfLDO2-DIu_f2H'),
-        'client_secret' => env('PAYPAL_LIVE_CLIENT_SECRET', 'EKHln5GyoivdoIW5U75KauXvnkiKLe0jBhBNg9BTWCME5osDP-fHKNSjO_uezZZR-NI5vQXbSMlAazFj'),
+        'client_id'     => env('PAYPAL_LIVE_CLIENT_ID', ''),
+        'client_secret' => env('PAYPAL_LIVE_CLIENT_SECRET', ''),
         'app_id'        => env('PAYPAL_LIVE_APP_ID', ''),
-        'webhook_id' => env('PAYPAL_WEBHOOK_ID', '15J86786BY168092A'),
-        'webhook_secret' => env('PAYPAL_WEBHOOK_SECRET', 'EOOlSBe8QdSvU9w9uFW2VnBktFV6KiUi5gWYLlPRozPrm9ul9itvMS519wr-FMZsiEogtyBJCHf5Ha7b'),
-        // 'merchant_id'   => env('PAYPAL_LIVE_MERCHANT_ID', 'T9NJ5KWJGKKL4'),
+        'webhook_id' => env('PAYPAL_WEBHOOK_ID', ''),
+        'webhook_secret' => env('PAYPAL_WEBHOOK_SECRET', ''),
+        // 'merchant_id'   => env('PAYPAL_LIVE_MERCHANT_ID', ''),
     ],
 
     'payment_action' => env('PAYPAL_PAYMENT_ACTION', 'Sale'),
